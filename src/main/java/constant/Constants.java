a0e1d8aac31e56b141b17074dd51efa9d498977c
dtygfn <1214948483@qq.com>
2019-02-14 16:28:34 +0800

    项目中用到的常量类

---
+++ src/main/java/constant/Constants.java
@@ -0,0 +1,35 @@
+package constant;
+
+/**
+ * 定义项目中用到的常量
+ */
+public interface Constants {
+    // 进行判断的参数
+    String REQUESTMODE = "requestmode";
+    String PROCESSNODE = "processnode";
+    String ISEFFECTIVE = "iseffective";
+    String ISBILLING = "isbilling";
+    String ISBID = "isbid";
+    String ISWIN = "iswin";
+    String ADORDEERID = "adorderid";
+    String WINPRICE = "winprice";
+    String ADPAYMENT = "adpayment";
+
+    // 统计的结果字段
+    String PROVINCENAME = "provincename";
+    String CITYNAME = "cityname";
+    String ORIGINAL_REQUEST_COUNT = "original_request_count";
+    String EFFECTIVE_REQUEST_COUNT = "effective_request_count";
+    String AD_REQUEST_COUNT = "ad_request_count";
+    String JOIN_BIDING_COUNT = "join_biding_count";
+    String BIDING_WIN_COUNT = "biding_win_count";
+    String BIDING_WIN_RATE = "biding_win_rate";
+    String SHOW_COUNT = "show_count";
+    String CLICK_COUNT = "click_count";
+    String CLICK_RATE = "click_rate";
+    String DSPWINPRICE = "DSPwinprice";
+    String DSPADPAYMENT = "DSPadpayment";
+
+
+
+}
